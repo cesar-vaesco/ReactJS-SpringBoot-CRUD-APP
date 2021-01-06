a5f7259f82344df20d08b498ef7fdb2c41e9674f
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-01-06 17:44:36 -0600

    Back:Creadno metodo que lista empleados por id

--- react-springboot-backend1/src/main/java/com/vaescode/backend/controller/EmpleadoController.java
+++ react-springboot-backend1/src/main/java/com/vaescode/backend/controller/EmpleadoController.java
@@ -3,13 +3,18 @@ package com.vaescode.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.vaescode.backend.exception.ResourceNotFoundException;
 import com.vaescode.backend.model.Empleado;
 import com.vaescode.backend.repository.EmpleadoRepository;
 
@@ -17,27 +22,34 @@ import com.vaescode.backend.repository.EmpleadoRepository;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmpleadoController {
-	
+
 	@Autowired
 	private EmpleadoRepository empleadoRepository;
-	
-	//listar todos los empleados
-	/*http://localhost:8080/api/v1/empleados*/
+
+	// listar todos los empleados
+	/* http://localhost:8080/api/v1/empleados */
 	@GetMapping("/empleados")
-	public List<Empleado> listarEmpleados(){
+	public List<Empleado> listarEmpleados() {
 		return empleadoRepository.findAll();
 	}
-	
-	//crear empleado rest api
-	/*http://localhost:8080/api/v1/empleados*/
-	/*{
-    "nombre":"***",
-    "apellido":"***",
-    "email":"****@correo.com"
-    }*/
+
+	// crear empleado rest api
+	/* http://localhost:8080/api/v1/empleados */
+	/*
+	 * { "nombre":"***", "apellido":"***", "email":"****@correo.com" }
+	 */
 	@PostMapping("/empleados")
 	public Empleado crearEmpleado(@RequestBody Empleado empleado) {
 		return empleadoRepository.save(empleado);
-	} 
+	}
+
+	// Listar empleado por id rest api
+	/**http://localhost:8080/api/v1/empleados/10*/
+	@GetMapping("/empleados/{id}")
+	public  ResponseEntity<Empleado> listarEmpleadoById(@PathVariable Long id) {
+		Empleado empleado = empleadoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Empleado no existe con el id: " + id));
+		return ResponseEntity.ok(empleado);
+	}
 
 }
